a2a4e08e6301cec3371fc0637e4aad2f8b9603ac
Taras Shpot <mrshpot@gmail.com>
2010-12-12 20:11:14 +0200

    Nicer message for EMMS notification

--- mrshpot/mode-customization/my-emms.el
+++ mrshpot/mode-customization/my-emms.el
@@ -1,4 +1,4 @@
 (defadvice emms-start (after emms-start-notify activate)
   "Send a notification when a track starts playing"
   (let ((fname (cdr (assoc 'name (emms-playlist-current-selected-track)))))
-	(notify "EMMS track started" fname)))
+	(notify "Currently playing:" fname)))
