7dad15a39462c8c33edc6a63e4fa0df791637c5b
Taras Shpot <mrshpot@gmail.com>
2011-07-25 14:50:37 +0300

    Add SLIME backend for auto-complete

--- mrshpot/mode-customization/my-slime.el
+++ mrshpot/mode-customization/my-slime.el
@@ -8,3 +8,15 @@
 	(slime-setup '(slime-fancy))
 	(setf common-lisp-hyperspec-root (concat "file://" emacs-root "html/CL-HyperSpec/"))
 	(return)))
+
+;; SLIME auto-complete integration
+(if (and (featurep 'slime) (featurep 'auto-complete))
+	(progn
+	  (message "Loading SLIME backend for auto-complete..")
+	  (add-site-lisp-dir "ac-slime-git")
+	  (require 'ac-slime)
+	  (add-hook 'slime-mode-hook 'set-up-slime-ac)
+	  (add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
+	  (add-to-list 'ac-modes 'slime-repl-mode)
+	  (add-to-list 'ac-modes 'lisp-mode))
+  (message "Skipping SLIME backend for auto-complete"))
