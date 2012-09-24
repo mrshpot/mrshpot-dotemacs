b11c596a35555f20dcc844bf52f2254e30324181
Taras Shpot <mrshpot@gmail.com>
2012-09-24 21:20:37 +0300

    Add insert-c-header-guard-uuid

--- mrshpot/helpers.el
+++ mrshpot/helpers.el
@@ -9,6 +9,10 @@
 
 ;;; Code:
 
+
+(require 'uuid)
+
+
 (defun insert-command-output (command)
   "Execute string COMMAND via \\[shell-command] and insert the output into the current buffer"
   (interactive
@@ -26,20 +30,34 @@
 	  (setf shell-output (buffer-string)))
 	(insert shell-output)))
 
-(defun insert-c-header-guard ()
-  "Insert C header guard."
+
+(defun insert-c-header-guard-internal (guard)
+  "Insert C header guard consisting of the ifdef/define/endif pattern."
+  (save-excursion
+	(goto-char (point-min))
+	(insert "#ifndef " guard)
+	(newline)
+	(insert "#define " guard)
+	(newline 2)
+	(goto-char (point-max))
+	(newline)
+	(insert "#endif // " guard)
+	(newline)))
+
+(defun insert-c-header-guard-simple ()
+  "Insert a C header guard in the form of FILE_NAME__INCLUDED"
   (interactive)
   (let ((guard
 		 (concat ""
-				 (upcase (replace-regexp-in-string "\\.\\|-" "_" (buffer-name)))
+				 (replace-regexp-in-string "\\.\\|-" "_" (buffer-name))
 				 "__INCLUDED")))
-	(save-excursion
-	  (goto-char (point-min))
-	  (insert "#ifndef " guard)
-	  (newline)
-	  (insert "#define " guard)
-	  (newline 2)
-	  (goto-char (point-max))
-	  (newline)
-	  (insert "#endif // " guard)
-	  (newline))))
+	(insert-c-header-guard-internal (upcase guard))))
+
+(defun insert-c-header-guard-uuid ()
+  "Insert C header guard in the form of _FILE_NAME_UUID_"
+  (interactive)
+  (let* ((guard-base (concat (buffer-name) "_" (uuid-1)))
+		 (guard (concat "_"
+						(replace-regexp-in-string "\\.\\|-" "_" guard-base)
+						"_")))
+	(insert-c-header-guard-internal (upcase guard))))
